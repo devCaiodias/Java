d7ff649f080c67f457fba7148eb6a3683e64f171
Caio Dias Marins <91338704+Caiodiasmartins@users.noreply.github.com>
2023-10-21 11:25:43 -0300

    Update Static.java

--- JavaEstudos/cursojava/stactic/Static.java
+++ JavaEstudos/cursojava/stactic/Static.java
@@ -2,7 +2,9 @@ package cursojava.stactic;
 
 public class Static {
 
-    //Os métodos static ou métodos da classe são funções que não dependem de nenhuma variável de instância, quando invocados executam uma função sem a dependência do conteúdo de um objeto ou a execução da instância de uma classe, conseguindo chamar direto qualquer método da classe e também manipulando alguns campos da classe.
+    /*Os métodos static ou métodos da classe são funções que não dependem de nenhuma variável de instância, quando invocados
+    executam uma função sem a dependência do conteúdo de um objeto ou a execução da instância de uma classe, conseguindo chamar 
+    direto qualquer método da classe e também manipulando alguns campos da classe.*/
     public static void main(String[] args) {
         int num=0;
         int pontos=2;
